d55b0c2e2678f4b8ab91335d76b739d0b7215f4e
soulhacker12-pec <155738175+soulhacker12-pec@users.noreply.github.com>
2024-03-08 22:06:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALLTYPECC")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DAXXSUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/bestfriend_Chatting_Hub")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/bestfriend_Chatting_Hub")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
