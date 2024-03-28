49ed78d130c7cf70b6a04b13206e0c284b5697e9
soulhacker12-pec <155738175+soulhacker12-pec@users.noreply.github.com>
2024-03-29 02:03:03 +0530

    Update wishcute.py

--- DAXXMUSIC/plugins/Yumi/wishcute.py
+++ DAXXMUSIC/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from DAXXMUSIC import app 
 
-SUPPORT_CHAT = "ALLTYPECC"
+SUPPORT_CHAT = "Soul_Network"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
