0444c30da0b8b7a3db27a66365fca54387347797
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-06-14 15:26:56 +0530

    Update test.py

--- DAXXMUSIC/plugins/tools/test.py
+++ DAXXMUSIC/plugins/tools/test.py
@@ -114,14 +114,15 @@ async def cmd_scr(client, message):
 [↯] 𝗣𝗿𝗼𝘅𝘆 ↳ Live ✅
 ➩ 𝗖𝗵𝗲𝗰𝗸𝗲𝗱 𝗕𝘆 : {le}
 """
-            await user.send_message(
-            chat_id=LOGS_CC,
-            text=card_caption,
-        )
-    except FloodWait as fw:
-        await asyncio.sleep(int(fw.value))
-    except Exception as e:
-        print(f"An error occurred: {e}")
+            try:
+                await user.send_message(
+                    chat_id=LOGS_CC,
+                    text=card_caption,
+                )
+            except FloodWait as fw:
+                await asyncio.sleep(int(fw.value))
+            except Exception as e:
+                print(f"An error occurred: {e}")
 
     try:
         if "https" in channel_link:
