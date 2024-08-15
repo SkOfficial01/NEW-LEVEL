80f7a7c82df63d4798389c7bc882273863cb29c3
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-08-15 19:29:30 +0530

    Update mongo.py

--- DAXXMUSIC/core/mongo.py
+++ DAXXMUSIC/core/mongo.py
@@ -1,22 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+
 from config import MONGO_DB_URI
+
 from ..logging import LOGGER
 
-logger = LOGGER(__name__)
-
-logger.info("Connecting to your Mongo Database...")
+LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
-    # Create the client instance
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    
-    # Access the desired database
-    mongodb = _mongo_async_.get_database()  # Update this to your database name if needed
-
-    # Test connection by executing a simple command
-    asyncio.run(mongodb.command('m'))  # Ensure MongoDB is reachable
-
-    logger.info("Connected to your Mongo Database.")
-except Exception as e:
-    logger.error(f"Failed to connect to your Mongo Database: {e}")
+    mongodb = _mongo_async_.Anon
+    LOGGER(__name__).info("Connected to your Mongo Database.")
+except:
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
     exit()
-    
